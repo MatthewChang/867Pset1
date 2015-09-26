3892955c9f07e476c7781fe4707f17d1ae4b8036
Matthew Chang <m_chang@mit.edu>
2015-09-26 14:01:38 -0400

    I did i again. Parker is going to do the extra stuff

--- 6867_hw1_data/homework1.py
+++ 6867_hw1_data/homework1.py
@@ -1,11 +1,19 @@
 import pdb
 import random
 import pylab as pl
+import numpy as np
 from scipy.optimize import fmin_bfgs
 
-# X is an array of N data points (one dimensional for now), that is, NX1
+# X is an array of N data points (one dimensional for now), that is, Nx1
 # Y is a Nx1 column vector of data values
 # order is the order of the highest order polynomial in the basis functions
+def designMatrix(X,order):
+    Z = X.T.tolist()[0]
+    return np.matrix([ [x**r for r in range(0,order+1)]  for x in Z]);
+
+def regressionFit(X,Y,phi):
+    return np.linalg.inv((phi.T*phi))*phi.T*Y
+
 def regressionPlot(X, Y, order):
     pl.plot(X.T.tolist()[0],Y.T.tolist()[0], 'gs')
 
@@ -19,8 +27,10 @@ def regressionPlot(X, Y, order):
     print 'w', w
     # produce a plot of the values of the function 
     pts = [[p] for p in pl.linspace(min(X), max(X), 100)]
-    Yp = pl.dot(w.T, designMatrix(pts, order).T)
+    A=  np.matrix(pts)
+    Yp = pl.dot(w.T, designMatrix(A, order).T)
     pl.plot(pts, Yp.tolist()[0])
+    pl.show()
 
 def getData(name):
     data = pl.loadtxt(name)
@@ -38,4 +48,7 @@ def regressTrainData():
 
 def regressValidateData():
     return getData('regress_validate.txt')
+
+X , Y = bishopCurveData();
+regressionPlot(X,Y,9);
 
